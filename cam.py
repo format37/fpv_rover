69e0fccc7d0660b75912abbed1a2a134513ab10a
Alexey Yurasov <13106906+format37@users.noreply.github.com>
2019-10-08 17:15:02 +0300

    Delete cam.py

--- cam.py
+++
@@ -1,9 +0,0 @@
-from picamera import PiCamera
-#import picamera
-from time import sleep 
-camera = PiCamera()
-camera.resolution = (1920, 1080)
-camera.start_preview()
-sleep(5)
-camera.capture('image.jpg')
-camera.stop_preview()
